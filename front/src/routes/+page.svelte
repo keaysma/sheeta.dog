d84a6336e12ed830f1f22ed1761c1c5ca4213afc
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2023-09-30 17:25:49 -0400

    Form cleanup

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,20 +1,17 @@
-<script lang="ts">
-</script>
-
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>sheeta.dog</title>
+	<meta name="description" content="SHEETA DOT DOG" />
 </svelte:head>
 
 <section id="home">
-	<h1>Woof</h1>
+	<h1>Woof.&nbsp;</h1>
 	<form method="get" action="game">
 		<fieldset>
-			<label for="name">Woof?</label>
-			<input id="name" name="name" placeholder="sheeta" />
+			<label for="name">Woof?</label><br/>
+			<input required id="name" name="name" placeholder="sheeta" />
 		</fieldset>
 
-		<button type="submit">woof!</button>
+		<button type="submit">woof!&nbsp;→</button>
 	</form>
 </section>
 
@@ -22,15 +19,28 @@
 	#home {
 		padding: 2em 4em;
 
+		> h1 {
+			text-decoration: underline;
+			font-size: 4em;
+		}
+
 		> form {
 			display: flex;
 			flex-direction: column;
 
-			width: 12em;
-			gap: 2em;
+			input {
+				padding: 0.5em;
+			}
 
-			button {
+			button[type="submit"] {
+				width: 8em;
+				height: 2em;
+
+				margin: 0.125em;
+				margin-top: 2em;
+
 				border: none;
+				border-radius: 0.5em;
 			}
 		}
 	}
